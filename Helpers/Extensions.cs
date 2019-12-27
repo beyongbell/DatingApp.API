58438760d901628d88e8a9b44d495944c059fe6d
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-12-27 13:33:02 +0700

    Add Helpers Error

---
+++ Helpers/Extensions.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace DatingApp.API.Helpers
+{
+    public static class Extensions
+    {
+        public static void AddApplicationError(this HttpResponse response, string message)
+        {
+            response.Headers.Add("Application-Error", message);
+            response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+    }
+}
