560f629ee937e2503f7ba468bb0d68ba88f78e7b
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-01-13 03:09:50 +0700

    Update Auth Photo

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
@@ -18,8 +19,10 @@ namespace DatingApp.API.Controllers
     {
         private readonly IAuthRepository _repository;
         private readonly IConfiguration _config;
-        public AuthController(IAuthRepository repository, IConfiguration config)
+        private readonly IMapper _mapper;
+        public AuthController(IAuthRepository repository, IConfiguration config, IMapper mapper)
         {
+            _mapper     = mapper;
             _config     = config;
             _repository = repository;
         }
@@ -69,9 +72,9 @@ namespace DatingApp.API.Controllers
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
-            return Ok(new {
-                token = tokenHandler.WriteToken(token)
-            });
+            var user = _mapper.Map<UserForListDto>(userFromRepository);
+
+            return Ok(new { token = tokenHandler.WriteToken(token), user });
         }
     }
 }
