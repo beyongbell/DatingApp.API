b685b6addb1121acefed2cb0675895a7b1cc07f1
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-01-13 17:41:11 +0700

    Add Pagination Users

--- Helpers/Extensions.cs
+++ Helpers/Extensions.cs
@@ -14,6 +14,15 @@ namespace DatingApp.API.Helpers
             response.Headers.Add("Access-Control-Allow-Origin", "*");
         }
 
+        public static void AddPagination(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader   = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            var camelCaseFormatter = new JsonSerializerSettings();
+            camelCaseFormatter.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader, camelCaseFormatter));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+
         public static int CalculateAge(this DateTime theDatetime)
         {
             var age = DateTime.Today.Year - theDatetime.Year;
