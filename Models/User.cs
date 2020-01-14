80dbe2c412b8cea37cc89fb793c474e6df49dade
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-01-14 14:44:22 +0700

    Create Like Model

--- Models/User.cs
+++ Models/User.cs
@@ -19,6 +19,8 @@ namespace DatingApp.API.Models
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public virtual ICollection<Photo> Photos { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+        public ICollection<Like> Likers { get; set; }
+        public ICollection<Like> Likees { get; set; }
     }
 }
