9101e559ebf42b91ac4996f92dea29884dd4f826
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-01-14 12:39:17 +0700

    Sorting LastActive

--- Data/DatingRepository.cs
+++ Data/DatingRepository.cs
@@ -44,7 +44,7 @@ namespace DatingApp.API.Data
 
         public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = _context.Users.Include(p => p.Photos).AsQueryable();
+            var users = _context.Users.Include(p => p.Photos).OrderByDescending(u => u.LastActive).AsQueryable();
 
             users = users.Where(u => u.Id != userParams.UserId);
 
@@ -56,6 +56,15 @@ namespace DatingApp.API.Data
                 var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
 
                 users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+            }
+
+            if (!string.IsNullOrEmpty(userParams.OrderBy))
+            {
+                switch (userParams.OrderBy)
+                {
+                    case "created": users = users.OrderByDescending(u => u.Created);    break;
+                    default:        users = users.OrderByDescending(u => u.LastActive); break;
+                }
             }
 
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
