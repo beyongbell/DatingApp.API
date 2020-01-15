2a23ce503a017f42a1ce05847eec99cffc14cc90
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-01-15 10:58:25 +0700

    Create Message Model

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -10,7 +10,7 @@ namespace DatingApp.API.Data
         public DbSet<User> Users { get; set;}
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
-
+        public DbSet<Message> Messages { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Like>().HasKey(k => new { k.LikerId, k.LikeeId });
@@ -25,6 +25,16 @@ namespace DatingApp.API.Data
                 .HasOne(u => u.Liker)
                 .WithMany(u => u.Likees)
                 .HasForeignKey(u => u.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(u => u.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(u => u.MessagesReceived)
                 .OnDelete(DeleteBehavior.Restrict);
         }
     }
