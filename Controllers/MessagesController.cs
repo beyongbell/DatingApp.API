f82f979804e41f9b2438ced240fa7d773905d7a9
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-01-17 07:27:57 +0700

    Create Message System

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Helpers;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [ServiceFilter(typeof(LogUserActivity))]
+    [Authorize]
+    [Route("api/users/{userId}/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IDatingRepository _repository;
+        private readonly IMapper _mapper;
+
+        public MessagesController(IDatingRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper     = mapper;
+        }
+
+        [HttpGet("{id}", Name = "GetMessage")]
+        public async Task<IActionResult> GetMessage(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var messageFromRepository = await _repository.GetMessage(id);
+
+            if (messageFromRepository == null)
+                return NotFound();
+
+            return Ok(messageFromRepository);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMessageForUser(int userId, MessageParams messageParams)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            messageParams.UserId = userId;
+
+            var messageFromRepository = await _repository.GetMessagesForUser(messageParams);
+
+            var messages = _mapper.Map<IEquatable<MessageToReturnDto>>(messageFromRepository);
+
+            Response.AddPagination(
+                messageFromRepository.CurrentPage,
+                messageFromRepository.PageSize,
+                messageFromRepository.TotalCount,
+                messageFromRepository.TotalPages
+            );
+
+            return Ok(messages);
+        }
+        [HttpGet("thread/{recipientId}")]
+        public async Task<IActionResult> GetMessageThread(int userId, int recipientId)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var messageFromRepository = await _repository.GetMessageThread(userId, recipientId);
+
+            var messageThread = _mapper.Map<IEquatable<MessageForReturnDto>>(messageFromRepository);
+
+            return Ok(messageThread);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            messageForCreationDto.SenderId = userId;
+
+            var recipient = await _repository.GetUser(messageForCreationDto.RecipientId);
+
+            if (recipient == null)
+                return BadRequest("Could not find user");
+
+            var message = _mapper.Map<Message>(messageForCreationDto);
+
+            _repository.Add(message);
+
+            if (await _repository.SaveAll())
+            {
+                var messageToReturn = _mapper.Map<MessageForReturnDto>(message);
+                return CreatedAtRoute("GetMessage", new {userId, id = message.Id}, messageToReturn);
+            }
+
+            throw new Exception("Creating the message failed on save");
+        }
+    }
+}
